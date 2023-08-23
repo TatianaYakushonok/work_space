06971a5e27c4695b15cc74ffbe8a38c3f4196d34
TatianaYakushonok <yakush2@yandex.ru>
2023-08-23 18:00:38 +0700

    feat: add data for select (Wed, 23 Aug 2023 18:00:37 +0700)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 // SELECT
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
 
-const citySelect = document.querySelector('#city');
-const cityChoices = new Choices(citySelect, {
-  searchEnabled: false,
-  itemSelectText: '',
-});
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    cbSuccess(data);
+  } catch (error) {
+    cbError(error);
+  }
+}
+
+/*fetch(API_URL + LOCATION_URL)
+  .then((response) => {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  })*/
 
 // MODAL
 
@@ -87,6 +104,21 @@ const modalController = ({ modalElem, btnOpen, btnClose, time = 300, open, close
 }
 
 const init = () => {
+
+  const citySelect = document.querySelector('#city');
+  const cityChoices = new Choices(citySelect, {
+    itemSelectText: '',
+  });
+
+  getData(`${API_URL}${LOCATION_URL}`, 
+    (locationData) => {
+      const locations = locationData.map(location => ({
+        value: location,
+      }));
+      cityChoices.setChoices(locations, 'value', 'label', true);
+    }, 
+    (err) => console.log(err));
+
   modalController({
     modalElem: '.modal',
     btnOpen: '.vacancy',
